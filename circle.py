0bd63f150f5a4889fb0fa64314927223f64f4719
BrianGio <38891888+BrianGio@users.noreply.github.com>
2018-09-17 13:09:54 -0400

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,12 @@
+"""
+File: circle.py
+Project 1.6v
+
+Compute and print area of circle if given an input radius
+"""
+
+#Single line comment
+
+radius = float(input("Please enter the radius >>"))
+area = 3.14 * radius ** 2
+print("The area is", area, "square units")
